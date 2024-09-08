8ffa443c00b1bf3dbdef289178e378ffd2f38ae0
Youssef Elaraby <y.ahmed.ea@gmail.com>
2024-09-08 18:37:08 +0300

    prepare to heroku

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,17 +1,22 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
+// path: ./config/env/production/database.js
+
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "strapi"),
-      password: env("DATABASE_PASSWORD", "strapi"),
-      schema: env("DATABASE_SCHEMA", "public"), // Not required
-      ssl: {
-        ca: env("DATABASE_CA"), // For self-signed certificates
+      client: "postgres",
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password,
+        ssl: { rejectUnauthorized: false },
       },
+      debug: false,
     },
-    debug: false,
-  },
-});
+  };
+};
